8978d29e46f9d47761d1bae43079458f6a53a296
HeegyeongJ <gmlrud0409@gmail.com>
2024-12-13 06:59:00 +0900

    v-for

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester']
+            variants: [{id:3234, color: 'green'}, {id:3235, color: 'blue'}]
         }
     }
 })
